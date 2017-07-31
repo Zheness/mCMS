a79c842539895b48b5defe183e67719899b6c878
Max PRUDHOMME <maxprudhomme@gmail.com>
2017-07-31 21:04:24 +0200

    Phalcon project created

---
+++ mcms/app/config/loader.php
@@ -0,0 +1,23 @@
+<?php
+
+use Phalcon\Loader;
+
+$loader = new Loader();
+
+/**
+ * Register Namespaces
+ */
+$loader->registerNamespaces([
+    'Mcms\Models' => APP_PATH . '/common/models/',
+    'Mcms'        => APP_PATH . '/common/library/',
+]);
+
+/**
+ * Register module classes
+ */
+$loader->registerClasses([
+    'Mcms\Modules\Frontend\Module' => APP_PATH . '/modules/frontend/Module.php',
+    'Mcms\Modules\Cli\Module'      => APP_PATH . '/modules/cli/Module.php'
+]);
+
+$loader->register();
